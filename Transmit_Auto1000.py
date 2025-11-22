c68e74207f303f205120abd4f2a71f31bc3ad337
Edd Palencia <79776448+EddSoul24@users.noreply.github.com>
2025-11-22 21:12:40 +1000

    Function to find transmittal excel file in use or template

--- Transmit_Auto1000.py
+++ Transmit_Auto1000.py
@@ -1,11 +1,23 @@
 import os
-import shutil
-import glob
 import re
-from openpyxl import load_workbook
-import win32com.client
-# from spire.xls import *
-# from spire.xls.common import *
+import shutil
+
+def find_excel_file():
+    print("Searching for Transmittal Excel file...")        
+    source_file = r"C:\Users\eddpa\Desktop\GoalFolder\Transmittal_TEMPLATE.xlsx" # Replace with template path used in company
+    rootDirectory = os.path.abspath(".")
+    rootKey = "."
+    transmittal_excel = "Transmittal.xlsx"
+    for currentDir, subDir, fileNames in os.walk(rootKey):
+        for file in fileNames:
+            transmittal_excel = os.path.join(currentDir, file)
+            if file == "Transmittal.xlsx" and currentDir != rootKey:
+                shutil.copy(transmittal_excel, rootDirectory)
+                print("Transmittal Excel file found and moved into root directory.")                
+                return os.path.join(rootDirectory, "Transmittal.xlsx")
+            if file == "Transmittal.xlsx" and currentDir == rootKey:
+                print("Transmittal Excel file found in root directory.")
+                return os.path.join(rootDirectory, "Transmittal.xlsx")
 
 
 if __name__ == "__main__":    
